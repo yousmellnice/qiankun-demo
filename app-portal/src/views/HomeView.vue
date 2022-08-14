54fa8b4210f2831646628d10d2469f935c465fb6
yushennan <yousmellnice@foxmail.com>
2022-08-15 02:27:49 +0800

    feat:new app-portal

---
+++ app-portal/src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<!--
+ * @Author: yushennan yousmellnice@foxmail.com
+ * @Date: 2022-08-15 02:12:13
+ * @LastEditors: yushennan yousmellnice@foxmail.com
+ * @LastEditTime: 2022-08-15 02:15:55
+ * @FilePath: /qiankun-ysn/qiankun-demo/app-portal/src/views/HomeView.vue
+ * @Description: 
+ * 
+-->
+<template>
+  <div class="home">
+    <h1>this is home page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  components: {
+  },
+})
+export default class HomeView extends Vue {}
+</script>
